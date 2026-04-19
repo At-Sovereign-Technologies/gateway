5ffe1159e81bb418267a3d85fa6860e5ba1950dc
DownloadableFox <me@downloadablefox.dev>
2026-04-19 00:56:06 -0500

    minor mistake in cascade sh

--- scripts/cascade.sh
+++ scripts/cascade.sh
@@ -1,5 +1,8 @@
 export $(cat .env | xargs)
 docker stack rm sello-legitimo
+
 echo "Waiting 30 seconds..."
+sleep 30
+
 git pull
 docker stack deploy -c production.yml sello-legitimo --with-registry-auth --resolve-image always
